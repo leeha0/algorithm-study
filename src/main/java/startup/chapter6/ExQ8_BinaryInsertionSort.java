6ada27ab71e591a97b6e73e7b88c6acf6f30e0fd
leeha0 <gkdud2837@gmail.com>
2022-02-13 23:43:23 +0900

    TODO: 이진 삽입 정렬 구현

---
+++ src/main/java/startup/chapter6/ExQ8_BinaryInsertionSort.java
@@ -0,0 +1,9 @@
+package startup.chapter6;
+
+public class ExQ8_BinaryInsertionSort {
+
+    // TODO 검색 파트 스터디 후 진행
+    public static void main(String[] args) {
+        System.out.println("이미 정렬된 부분에 이진 검색을 사용하여 삽입 위치를 더 빨리 찾을 수 있다.");
+    }
+}
